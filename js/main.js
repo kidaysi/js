960f463f0657d2c66e8e50432ef8af5b7bce86ac
kidaysi <frvrevrlana@gmail.com>
2023-09-08 22:34:55 +0300

    sm3

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
-let city=prompt('Введите название города');
-let year=prompt('Введите год образования');
-let people=prompt('Введите кол-во населения города');
-alert('Городу ' + city + ' исполнилось ' +(2023-Number(year)) + ' лет с момента образования. Население города насчитывает ' + Number(people) + ' человек.');
+// let city=prompt('Введите название города');
+// let year=prompt('Введите год образования');
+// let people=prompt('Введите кол-во населения города');
+// alert('Городу ' + city + ' исполнилось ' +(2023-Number(year)) + ' лет с момента образования. Население города насчитывает ' + Number(people) + ' человек.');
+
+let price = prompt('Введите стоимость товара');
+let count = prompt('Введите количество денег клиента');
 
 
-
+if (price === count) {
+    alert("Покупка совершена");
+} else if (price > count) {
+    let shortage = price - count;
+    alert("Для покупки не хватает " +shortage+ " ₽");
+    console.log(shortage)
+} else{
+    let hit = count - price;
+    alert("Покупка совершена. Сдача " +hit+ " ₽");
+}
