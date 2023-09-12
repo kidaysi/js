4f793120ac07631077fea1412786a3d04551146d
kidaysi <frvrevrlana@gmail.com>
2023-09-12 16:40:05 +0300

    sm4

--- js/main.js
+++ js/main.js
@@ -2,15 +2,28 @@
 // let year=prompt('Введите год образования');
 // let people=prompt('Введите кол-во населения города');
 // alert('Городу ' + city + ' исполнилось ' +(2023-Number(year)) + ' лет с момента образования. Население города насчитывает ' + Number(people) + ' человек.');
-//
 
-let price = +prompt('Введите стоимость товара')
-let count = +prompt('Введите количество денег клиента')
 
-if(price === count) {
-    alert("Покупка совершена")
-}else if (price > count){
-    alert (`Покупка не совершена. Не хватает ${price - count}₽`);
-}else{
-    alert(`Покупка совершена. Сдача ${count - price} ₽`);
+// let price = +prompt('Введите стоимость товара')
+// let count = +prompt('Введите количество денег клиента')
+// let a = document.querySelector(".itog")
+// if(price === count) {
+//     let ok = "Покупка совершена";
+//     a.textContent=ok;
+// }else if (price > count){
+//     let no = `Покупка не совершена. Не хватает ${price - count} ₽`;
+//     a.textContent=no;
+// }else{
+//     let lok = `Покупка совершена. Сдача ${count - price} ₽`;
+//     a.textContent=lok;
+// }
+
+
+let n = +prompt("Введите кол-во блоков");
+let box = '<div class="box"></div>';
+let box_list = document.querySelector('.box_list');
+
+for(let y = 0; y = n; y++){
+    box_list.insertAdjacentHTML('beforeend',box);
 }
+
